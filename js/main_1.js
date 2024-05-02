2e27a3e47ef9d2803ab21c852d5e4919401e8a05
Your <rlawlsdn1020@gmail.com>
2024-05-02 17:47:25 +0900

    first commit

---
+++ js/main_1.js
@@ -0,0 +1,6 @@
+$(" ul > li").mouseenter(function () {
+  $(this).children(".list1").stop().slideDown(400);
+});
+$(" ul > li").mouseleave(function () {
+  $(this).children(".list1").stop().slideUp(100);
+});
