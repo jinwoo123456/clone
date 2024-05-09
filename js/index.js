c6499f970079bf17e5480a466ad76911f1f8b21e
Your <rlawlsdn1020@gmail.com>
2024-05-09 15:13:32 +0900

    0509clone

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
+let list1 = document.querySelector(".list1");
+
+let hd = () => {
+  $(".list1").hide();
+};
+
 $(" ul > li").mouseenter(function () {
   $(this).children(".list1").stop().slideDown(400);
+  $(this).children(".list1").show();
 });
 $(" ul > li").mouseleave(function () {
   $(this).children(".list1").stop().slideUp(100);
 });
+
+hd();
 AOS.init();
